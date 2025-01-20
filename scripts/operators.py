f13ea5841ff25dd53b79c9f42f72e388394a7453
cykillon3 <110654808+cykillon3@users.noreply.github.com>
2025-01-20 18:01:56 +0500

    Fixed a bug.
    Fixed a bug where spawning a new hair would be controlled by the original hair shape.

--- scripts/operators.py
+++ scripts/operators.py
@@ -15,14 +15,30 @@ class create_curve(operator):
         return True
 
     def execute(self, context):
-        bpy.ops.curve.primitive_nurbs_circle_add(rotation=(1.5708, 0, 0))
-        bpy.context.object.name = "Hair"
+        
+        hair_id = len(bpy.data.objects)
+        
+        bpy.ops.curve.primitive_bezier_circle_add(rotation=(1.5708, 0, 0))
+        hair_shape = bpy.context.object
+        hair_shape.name = f"Hair Shape{hair_id}"
+        hair_shape.data.resolution_u = 5
+        
+        
+        bpy.ops.curve.primitive_nurbs_path_add(location=(3, 0, 0), rotation=(0, 0, 1.5708))
+        hair_strand = bpy.context.object
+        hair_strand.name = f"Hair Strand{hair_id}"
+        hair_strand.data.resolution_u = 3
+        hair_strand.data.bevel_mode = 'OBJECT'
+        hair_strand.data.bevel_object = hair_shape
+        hair_strand.data.splines[0].resolution_u = 3
+
         return {"FINISHED"}
 
 
 class edit_curve(operator):
     bl_idname = "edit_curve.curve_operator01"
     bl_label = "Edit hair"
+    
     bl_description = "Edits created hair"
     bl_options = {"REGISTER", "UNDO"}
 
@@ -31,18 +47,23 @@ class edit_curve(operator):
         return True
 
     def execute(self, context):
-        bpy.ops.object.select_all(action='DESELECT')
+        #bpy.ops.object.select_all(action='DESELECT')
         
         # Select the curve object (assuming the curve is named "NurbsCircle")
-        curve_object = bpy.data.objects.get("Hair")
+        curve_object = bpy.context.view_layer.objects.active
         if curve_object:
             curve_object.select_set(True)
             context.view_layer.objects.active = curve_object
+            
+            # Check if the object is in 'EDIT' mode
+            if bpy.context.object.mode == 'EDIT':
+                bpy.ops.object.mode_set(mode='OBJECT')
             
             # Set the mode to 'EDIT'
             bpy.ops.object.mode_set(mode='EDIT')
             bpy.ops.view3d.view_axis(type='FRONT', align_active=False)
             bpy.context.space_data.region_3d.view_perspective = 'ORTHO'
+
         return {"FINISHED"}
 
 
